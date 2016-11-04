d5231e5326820a304f93f88eab6269f99319dfd5
Juan Carlos Barrientos <jcbarrientos946@gmail.com>
2016-11-04 18:49:37 +0100

    Basico

---
+++ Funcion11.py
@@ -0,0 +1,9 @@
+def qq(num1,num2):
+	Suma=0
+	if num1 and num2:
+		Suma=num1+num2
+	return Suma
+
+print qq(5,5)
+print qq(2,5)
+print qq(8,8)
--- funcion3.py
+++
@@ -1,9 +0,0 @@
-def cadena(cad):
-	c=cad
-	cont=0
-	while cont <9:
-		print c
-		cont+=1
-	return c
-	
-print cadena('pako')
