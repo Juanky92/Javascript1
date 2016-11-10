337c7c9c69bd79a5fff3eb97bca9859b2f26285b
Juan Carlos <jcbarrientos946@gmail.com>
2016-11-10 01:33:35 +0100

    Basicos2

--- Funcion5.py
+++
@@ -1,7 +0,0 @@
-def tt(lista):
-	Suma=0
-	for ele in lista:
-		Suma+=ele
-	return Suma
-	
-print tt([1,9,7,5,6,5])
--- Funcion7.py
+++
@@ -1,8 +0,0 @@
-def p(lista):
-	c=lista
-	for ele in lista:
-		lista.reverse()
-	return c
-
-print p([1,2,5,4,6])
-print p([6,5,4,3,2,1,0])
